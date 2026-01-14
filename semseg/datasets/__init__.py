c44c7285e041e2fcfe33b9b17162a476f5b5fb21
LeonardoNotari <67100695+LeonardoNotari@users.noreply.github.com>
2026-01-14 17:50:57 +0100

    Add files via upload

---
+++ semseg/datasets/__init__.py
@@ -0,0 +1,11 @@
+from .mcubes import MCubeS
+from .fmb import FMB
+from .pst import PST
+from .mortars import MORTARS
+
+__all__ = [
+    'MCubeS',
+    'FMB',
+    'PST',
+    'MORTARS',
+]
---
+++ semseg/datasets/mortars.py
@@ -0,0 +1,127 @@
+import os
+import torch
+from torch import Tensor
+from torch.utils.data import Dataset
+import torchvision.transforms.functional as TF
+from pathlib import Path
+from typing import Tuple
+import glob
+from torch.utils.data import DataLoader
+from semseg.augmentations_mm import get_train_augmentation
+from PIL import Image
+import numpy as np
+import random
+
+
+
+class MORTARS(Dataset):
+    """
+    Dataset per immagini mortars con 2 canali RGB e label con 3 classi + ignore
+    """
+    CLASSES = ["Legante", "Porosità", "Aggregati"]
+    
+    PALETTE = torch.tensor([
+        [128, 0, 0],
+        [0, 128, 0],
+        [0, 0, 128]
+    ])
+
+    def __init__(
+        self,
+        root: str = 'data/mortars',
+        split: str = 'train',
+        transform=None,
+        modals=['paralleli', 'incrociati'],
+        case=None,
+        val_ratio=0.20,
+        test_ratio=0.20,
+        seed=42
+    ):
+        super().__init__()
+        assert split in ['train', 'val', 'test']
+
+        self.transform = transform
+        self.modals = modals
+        self.n_classes = len(self.CLASSES)
+        self.ignore_label = 3
+
+        # --------------------------------------------------
+        # 1. carico TUTTI i file dal dataset unico
+        # --------------------------------------------------
+        all_files = sorted(
+            glob.glob(os.path.join(root, modals[0], '*.tif'))
+        )
+
+        if not all_files:
+            raise RuntimeError(f"No images found in {os.path.join(root, modals[0])}")
+
+        # --------------------------------------------------
+        # 2. split riproducibile
+        # --------------------------------------------------
+        random.seed(seed)
+        random.shuffle(all_files)
+
+        n = len(all_files)
+        n_test = int(test_ratio * n)
+        n_val  = int(val_ratio * n)
+        n_train = n - n_val - n_test
+
+        if split == 'train':
+            self.files = all_files[:n_train]
+        elif split == 'val':
+            self.files = all_files[n_train:n_train + n_val]
+        else:  # test
+            self.files = all_files[n_train + n_val:]
+
+        print(f"[MORTARS] {split}: {len(self.files)} samples")
+    
+    
+    def _open_img(self, file):
+        img = Image.open(file).convert('RGB')  # forza RGB
+        img = TF.to_tensor(img)  
+        img = (img * 255).byte() 
+        return img
+    
+    def _open_label(self, file):
+        lbl = Image.open(file)
+        lbl = torch.from_numpy(np.array(lbl)).long()  # [H,W]
+        return lbl.unsqueeze(0)  # [1,H,W]
+
+    def __len__(self) -> int:
+        return len(self.files)
+    
+    def __getitem__(self, index: int) -> Tuple[list, Tensor]:
+        sample = {}
+
+        # apro le immagini dei modali
+        for modal in self.modals:
+            img_path = self.files[index].replace(self.modals[0], modal)
+            img = self._open_img(img_path)
+            sample[modal] = img
+
+        # apro la label
+        lbl_path = self.files[index].replace(self.modals[0], 'label')
+        label = self._open_label(lbl_path)
+        label[label == 255] = self.ignore_label
+        sample['mask'] = label
+
+        # aggiungo chiave 'img' per compatibilità con il transform
+        sample['img'] = sample[self.modals[0]]
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        label = sample.pop('mask')
+        label = label.squeeze(0)  
+        sample_list = [sample[m] for m in self.modals]
+
+        return sample_list, label.long()
+
+
+if __name__ == '__main__':
+    traintransform = get_train_augmentation((512, 512))
+    trainset = MORTARS(transform=traintransform, split='train')
+    trainloader = DataLoader(trainset, batch_size=2, num_workers=2, drop_last=False, pin_memory=False)
+
+    for i, (sample, lbl) in enumerate(trainloader):
+        print(torch.unique(lbl))
